7a85b3ae2a5c0699b5197f062352f7969399eaf9
Shail Macwan <119092769+Shail-26@users.noreply.github.com>
2024-08-15 12:02:04 +0530

    Add the functionality of creation and login of admin + admin have the access of all the customer info

--- root/backend/app.js
+++ root/backend/app.js
@@ -12,6 +12,7 @@ app.use(express.json())
 
 app.use(cors())
 app.use('/api/auth', require('./routes/auth'))
+app.use('/api/admin', require('./routes/customers'))
 
 app.get('/', (req, res)=>{
     res.send("<h1>Hello World</h1>")
