4dc219673c6aa357e4891a4bfb137d735f57d761
22cs044Shruti <22cs044@charusat.edu.in>
2024-08-18 15:57:25 +0530

    Home page Updataed

--- root/frontend/src/components/Home.js
+++ root/frontend/src/components/Home.js
@@ -2,12 +2,22 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import Navbar from './Navbar';
+import '../assets/styles/Home.css';
+import '../assets/styles/forall.css';
+import CmLens from '../assets/images/CamLens.png';
 
 const HomePage = () => {
   return (
-    <div>
+    <div className='navbar-div'>
       <Navbar />
-      
+      <div className='container-home'>
+        <div className="text-container">
+          <h1 className="animated-text">Welcome To the Art of Memories</h1>
+        </div>
+        <div className="lens-container">
+          <img src={CmLens} alt="Camera Lens" className="camera-lens" />
+        </div>
+      </div>
     </div>
   );
 };
