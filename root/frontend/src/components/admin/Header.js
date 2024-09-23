652c22fdfe23093f2b63f79c9a6c06f77e364609
22cs044Shruti <22cs044@charusat.edu.in>
2024-09-23 11:03:11 +0530

    Header css is changed

--- root/frontend/src/components/admin/Header.js
+++ root/frontend/src/components/admin/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../assets/styles/forall.css';
 import logo from '../../assets/images/Logo img.png';
+import profilePic from '../../assets/images/festival.jpg'; // Add your profile picture image here
 import { useNavigate } from 'react-router-dom';
 import '../../assets/styles/Header.css';
 
@@ -11,13 +12,20 @@ const Navbar = () => {
         localStorage.removeItem('admin');
         navigate('/home')
     };
+
     return (
         <div className="navbar admin">
             <div className="nav-cont">
                 <div className="logo">
                     <img src={logo} alt="Logo" />
                 </div>
-                    <p>Hello Owner </p>
+
+                {/* Profile section */}
+                <div className="profile-section">
+                    <img src={profilePic} alt="Profile" className="profile-pic" />
+                    <p>Hello Owner!</p>
+                </div>
+
                 <div className='btn-main-class'>
                     <div className='button-class'>
                         <button className='sign-in-btn nav-btn'><a onClick={handleLogout}>Logout</a></button>
