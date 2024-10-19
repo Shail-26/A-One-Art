ca2fc6d5a9e0ee80202ad8c0f2f08e3af895657e
Shail Macwan <119092769+Shail-26@users.noreply.github.com>
2024-10-19 10:06:54 +0530

    Refactor AboutUs component to include owner details and adjust contact information styles

--- root/frontend/src/components/AboutUs.js
+++ root/frontend/src/components/AboutUs.js
@@ -16,7 +16,6 @@ const AboutUs = () => {
                     <div className="achievements-grid">
                         <div className="achievement-item">
                             <img src={achievement1} alt="Achievement 1" />
-                            <p>Description of Achievement 1</p>
                         </div>
                         <div className="achievement-item">
                             <img src="achievement2.jpg" alt="Achievement 2" />
@@ -36,16 +35,23 @@ const AboutUs = () => {
                 <div className="section owner-details">
                     <h2>Owner Details</h2>
                     <div className="contact-info">
-                        <p><strong>Email:</strong> <a href="mailto:owner@example.com">owner@example.com</a></p>
-                        <p><strong>Phone No.:</strong> <a href="tel:+1234567890">+1234567890</a></p>
+                        <p class="about-mail"><strong>Email:</strong> <a href="mailto:aoneart2008@gmail.com">aoneart2008@gmail.com</a></p>
+                        <p class="about-tel"><strong>Phone No.:</strong> <a href="tel:+91 9979534982">+91 9979534982</a></p>
                     </div>
                 </div>
 
                 <div className="section studio-map">
                     <h2>Studio Details</h2>
                     <div className="map-container">
-                        <p>Map of the Shop</p>
-                        {/* Embed Google Map or place a static map image here */}
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3681.0027666554724!2d72.86056151129715!3d22.69094092853582!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjLCsDQxJzI3LjQiTiA3MsKwNTEnNDcuMyJF!5e0!3m2!1sen!2sin!4v1729311336446!5m2!1sen!2sin"
+                            width="100%"
+                            height="500"
+                            style={{ border: 0 }}
+                            allowFullScreen=""
+                            loading="lazy"
+                            referrerPolicy="no-referrer-when-downgrade"
+                        ></iframe>
                     </div>
                 </div>
             </div>
